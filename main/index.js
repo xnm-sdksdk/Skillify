0ce5d4ce06158806b0c7ce61df9ca92441df2bc1
Nuno Mendonça <84451848+xnm-sdksdk@users.noreply.github.com>
2022-06-15 12:02:23 +0100

    Add files via upload

---
+++ main/index.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+    window.addEventListener('scroll', () => {
+        if(window.pageYOffset > 100) {
+            document.querySelector('header').classList.add('is-scrolling');
+        } else {
+            document.querySelector('header').classList.remove('is-scrolling')
+        }
+    });
+}
+
+const menuBtn = document.querySelector('.menu');
+
+const mobile_menu = document.querySelector('mobile-nav');
+
+
+    menuBtn.addEventListener('click', () => {
+    menuBtn.classList.add('is-active');
+})
+
+function openSlideMenu() {
+    document.getElementById('nav_menu').style.width = '414px';
+    document.getElementById('content_menu').style.marginRight = '400px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('nav_menu').style.width = '0x';
+    document.getElementById('content_menu').style.marginRight = '0px';
+}
