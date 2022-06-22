1c75a7204a8a83c4aabea3b654b07f7eeef95986
TiagoRibeiro25 <40210462@esmad.ipp.pt>
2022-06-22 11:04:54 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,38 +1,36 @@
 window.onload = () => {
-    window.addEventListener('scroll', () => {
-        if(window.pageYOffset > 100) {
-            document.querySelector('header').classList.add('is-scrolling');
-        } else {
-            document.querySelector('header').classList.remove('is-scrolling')
-        }
-    });
-}
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+      document.querySelector("header").classList.add("is-scrolling");
+    } else {
+      document.querySelector("header").classList.remove("is-scrolling");
+    }
+  });
+};
 
-const menuBtn = document.querySelector('.menu');
+/* const menuBtn = document.querySelector(".menu"); */
 
-const mobile_menu = document.querySelector('mobile-nav');
+const mobile_menu = document.querySelector("mobile-nav");
 
-
-    menuBtn.addEventListener('click', () => {
-    menuBtn.classList.add('is-active');
-})
+/* menuBtn.addEventListener("click", () => {
+  menuBtn.classList.add("is-active");
+}); */
 
 function openSlideMenu() {
-    document.getElementById('nav_menu').style.width = '414px';
-    document.getElementById('content_menu').style.marginRight = '400px';
+  document.getElementById("nav_menu").style.width = "414px";
+  document.getElementById("content_menu").style.marginRight = "400px";
 }
 
 function closeSlideMenu() {
-    document.getElementById('nav_menu').style.width = '0x';
-    document.getElementById('content_menu').style.marginRight = '0px';
+  document.getElementById("nav_menu").style.width = "0x";
+  document.getElementById("content_menu").style.marginRight = "0px";
 }
 
-
 // GO UP BUTTON
-const go_up = document.getElementById('go_up');
-go_up.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-})
+const go_up = document.getElementById("go_up");
+go_up.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
